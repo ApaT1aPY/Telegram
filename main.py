4ca9783a7296c9d1783481f0acd116fd6816b0a4
ApaTia Python <108146296+ApaT1aPY@users.noreply.github.com>
2024-06-26 20:04:21 +0300

    Долой комментарии

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-# Импорт необходимых библиотек и модулей
 import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters.command import Command
-from config import *  # Не забудь записать токен в переменную TOKEN_BOT в файле config.py =)
+from config import *
 
 
-# Включить логирование на уровне INFO
 logging.basicConfig(level=logging.INFO)
 
 
-# Создать объект бота
 bot = Bot(token=TOKEN_BOT)
-
-# Создать диспетчер
 dp = Dispatcher()
 
 
-# Обработчик команды /start
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    # Отправить приветствие с форматированным именем пользователя
+async def start_command(message: types.Message):
+    await message.delete()
     await message.answer(f"Привет <b>{message.from_user.username}</b>!", parse_mode=ParseMode.HTML)
+    await asyncio.sleep(30)
+    await bot.delete_message(message.chat.id, message.message_id)
 
 
-# Функция для запуска процесса поллинга новых обновлений
 async def main():
-    # Запустить процесс поллинга новых обновлений
     await dp.start_polling(bot)
 
 
-# Если файл запущен как главный
 if __name__ == "__main__":
-    # Запустить процесс обновления в асинхронном режиме
     asyncio.run(main())
